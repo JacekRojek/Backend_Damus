7d73c33bfe8191b24ef52c2d02c2dee2511b4b2f
Jacek Rojek <j.rojek19@gmail.com>
2017-07-23 14:52:16 +0100

    create method added

--- routes/products.js
+++ routes/products.js
@@ -3,11 +3,21 @@ var router = express.Router();
 var db = require('../bin/www');
 
 /* GET users listing. */
+
 router.get('/', function(req, res, next) {
   var database = db.db();
   var cursor = database.collection('products').find().toArray(function(err, results) {
-  res.send(results);
-})
+    res.send(results);
+  })
 });
+
+router.post('/', (req, res) => {
+    var database = db.db();
+    database.collection('products').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
 
 module.exports = router;
